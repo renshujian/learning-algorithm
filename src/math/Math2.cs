8bd5eeadd2ff8e938bf18916ed1b9836037c53eb
James Ren <renshujian0@hotmail.com>
2023-03-21 22:25:44 +0800

    Add Convert2

---
+++ src/math/Math2.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithm
+{
+    public class Math2
+    {
+        public const uint BASE = 2;
+
+        public static string Convert2(uint value)
+        {
+            uint[] result = new uint[32];
+            for (int i = 31; i >= 0; i--)
+            {
+                uint mod = value % BASE;
+                result[i] = mod;
+                if (value < BASE)
+                {
+                    break;
+                }
+                else
+                {
+                    value = value / BASE;
+                }
+            }
+            return string.Join("", result);
+        }
+    }
+}
---
+++ test/math/Math2Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm
+{
+    [TestClass]
+    public class Math2Test
+    {
+        [TestMethod]
+        [DataRow(0u, "00000000000000000000000000000000")]
+        [DataRow(1u, "00000000000000000000000000000001")]
+        [DataRow(2u, "00000000000000000000000000000010")]
+        [DataRow(255u, "00000000000000000000000011111111")]
+        [DataRow(256u, "00000000000000000000000100000000")]
+        [DataRow(65535u, "00000000000000001111111111111111")]
+        [DataRow(65536u, "00000000000000010000000000000000")]
+        [DataRow(uint.MaxValue, "11111111111111111111111111111111")]
+        public void Convert2(uint base10, string base2)
+        {
+            Assert.AreEqual(base2, Math2.Convert2(base10));
+        }
+    }
+}
